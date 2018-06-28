0d64b0e03df582ceab715197dfb368bc32df279e
Kevin4230623 <mikechen31@kimo.com>
2018-06-28 10:38:10 +0800

    first commit

---
+++ Python36/speech_test.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+	print("Please wait. Calibrating microphone...")
+	r.adjust_for_ambient_noise(source, duration=5)
+	print("Say something!")
+	audio=r.listen(source)
+
+try:
+   print("Google Speech Recognition thinks you said:")
+   print(r.recognition_google(audio, language="zh_TW"))
+except sr.UnknownValueError:
+	print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+	print("No reponse from Google Speech Recognition service:{0}".format(e))
+
